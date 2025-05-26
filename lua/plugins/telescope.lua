b6c3864b67958d54ce4b8984dedde141432087b5
Michael Timko <michael.timko@ext.chime.com>
2025-05-26 17:40:32 -0400

    Clean up

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,25 +1,13 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  dependencies = {
-    {
-      "nvim-telescope/telescope-live-grep-args.nvim",
-      -- This will not install any breaking changes.
-      -- For major updates, this must be adjusted manually.
-      version = "^1.0.0",
-    },
-  },
+  "nvim-telescope/telescope-live-grep-args.nvim",
   keys = {
     { "<leader>\\", "<cmd>Telescope live_grep_args<cr>", desc = "Command History" },
   },
   config = function()
-    local telescope = require("telescope")
+    LazyVim.on_load("telescope.nvim", function()
+      local telescope = require("telescope")
 
-    -- first setup telescope
-    -- telescope.setup({
-    --   -- your config
-    -- })
-
-    -- then load the extension
-    telescope.load_extension("live_grep_args")
+      telescope.load_extension("live_grep_args")
+    end)
   end,
 }
