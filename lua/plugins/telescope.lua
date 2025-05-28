cfa645776a711a598eca16c609e8676d63f08f8e
Michael Timko <michael.timko@ext.chime.com>
2025-05-27 21:48:30 -0400

    Config telescope to use vertical layout and file name first

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,13 +1,26 @@
 return {
-  "nvim-telescope/telescope-live-grep-args.nvim",
-  keys = {
-    { "<leader>\\", "<cmd>Telescope live_grep_args<cr>", desc = "Command History" },
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = {
+      pickers = {
+        find_files = {
+          path_display = { "filename_first" },
+          layout_strategy = "vertical",
+        },
+      },
+    },
   },
-  config = function()
-    LazyVim.on_load("telescope.nvim", function()
-      local telescope = require("telescope")
+  {
+    "nvim-telescope/telescope-live-grep-args.nvim",
+    keys = {
+      { "<leader>\\", "<cmd>Telescope live_grep_args<cr>", desc = "Command History" },
+    },
+    config = function()
+      LazyVim.on_load("telescope.nvim", function()
+        local telescope = require("telescope")
 
-      telescope.load_extension("live_grep_args")
-    end)
-  end,
+        telescope.load_extension("live_grep_args")
+      end)
+    end,
+  },
 }
