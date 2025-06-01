d294cfba7a07f66055f9c139a6626de370b38686
Michael Timko <timkomip@gmail.com>
2025-06-01 18:26:38 -0400

    Autocmd that fixes ruby period indent issue

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,15 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- vim.cmd('autocmd FileType ruby setlocal indentkeys-=.')
+--
+
+local ruby_indent_fix = vim.api.nvim_create_augroup("Ruby Indent Keys Fix", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "ruby",
+  callback = function()
+    vim.cmd("setlocal indentkeys-=.")
+  end,
+  group = ruby_indent_fix,
+})
